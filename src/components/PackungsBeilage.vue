9ec4236b6a038a6a49dbaba19b42ee904c6e83e7
TKUU95 <timo.kraemer@uni-ulm.de>
2022-01-31 14:07:31 +0100

    ich bin so gut!

--- src/components/PackungsBeilage.vue
+++ src/components/PackungsBeilage.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <v-dialog
-      transition="dialog-top-transition"
-      v-model="show"
-      fullscreen
-      hide-overlay
-    >
+    <v-dialog transition="dialog-top-transition" v-model="show" fullscreen hide-overlay>
       <template v-slot:default="dialog">
         <v-card>
           <v-toolbar dark color="primary">
@@ -16,43 +11,57 @@
             <v-spacer></v-spacer>
           </v-toolbar>
           <v-form ref="form" @submit.prevent="submit">
-            <v-expansion-panels
-              v-for="(key, index) in filteredKeys"
-              :key="index"
-              v-model="panel"
-              multiple
-            >
-              <v-expansion-panel :class="{ 'blue--text': sortBy === key }">
-                <v-expansion-panel-header>Anwendung</v-expansion-panel-header>
-                <v-expansion-panel-content>
-                   
+            <v-expansion-panels>
+              <v-expansion-panel v-for="(key, index) in sortedLeaflet" :key="index" multiple>
+                <v-expansion-panel-header>{{ capitalizeFirstLetter(index) }}</v-expansion-panel-header>
+                <v-expansion-panel-content v-if="index == 'ref'">
+                  Für mehr Informationen <a target="_blank" v-bind:href="displayLink(key)">klicken sie hier!</a>
                 </v-expansion-panel-content>
-              </v-expansion-panel>
-
-              <v-expansion-panel>
-                <v-expansion-panel-header>Verwendung</v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  Some content
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-              <v-expansion-panel>
-                <v-expansion-panel-header>Dosierung</v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  Some content
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-              <v-expansion-panel>
-                <v-expansion-panel-header>Nebenwirkungen</v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  Some content
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-              <v-expansion-panel>
-                <v-expansion-panel-header>
-                  {{ leaflet }}
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  Some content
+                <v-expansion-panel-content v-else>
+                  <template v-if="Array.isArray(key)">
+                    <v-list disabled>
+                      <v-list-item v-for="(item, i) in key" :key="i">
+                        <v-list-item-content>
+                          <v-list-item-title v-text="item"></v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list>
+                  </template>
+                  <v-expansion-panels v-else-if="typeof key === 'object' && key !== null">
+                    <v-expansion-panel v-for="(key2, index2) in key" :key="index2" multiple>
+                      <v-expansion-panel-header>{{ capitalizeFirstLetter(index2) }}</v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        <template v-if="Array.isArray(key2)">
+                          <v-list disabled>
+                            <v-list-item v-for="(item2, i2) in key2" :key="i2">
+                              <v-list-item-content>
+                                <v-list-item-title v-text="item2"></v-list-item-title>
+                              </v-list-item-content>
+                            </v-list-item>
+                          </v-list>
+                        </template>
+                        <v-expansion-panels v-else-if="typeof key2 === 'object' && key2 !== null">
+                          <v-expansion-panel v-for="(key3, index3) in key2" :key="index3" multiple>
+                            <v-expansion-panel-header>{{ capitalizeFirstLetter(index3) }}</v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                              <template v-if="Array.isArray(key3)">
+                          <v-list disabled>
+                            <v-list-item v-for="(item3, i3) in key3" :key="i3">
+                              <v-list-item-content>
+                                <v-list-item-title v-text="item3"></v-list-item-title>
+                              </v-list-item-content>
+                            </v-list-item>
+                          </v-list>
+                        </template>
+                              <p v-else>{{ key }}</p>
+                            </v-expansion-panel-content>
+                          </v-expansion-panel>
+                        </v-expansion-panels>
+                        <p v-else>{{ key2 }}</p>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                  <p v-else>{{ key }}</p>
                 </v-expansion-panel-content>
               </v-expansion-panel>
             </v-expansion-panels>
@@ -64,27 +73,43 @@
 </template>
 
 <script>
-export default {
-  props: {
-    value: Boolean,
-    leaflet: null,
-  },
-  data: () => ({}),
-  computed: {
-    filteredKeys() {
-        return this.keys.filter((key) => key !== "Name");
+  export default {
+    props: {
+      value: Boolean,
+      leaflet: null,
     },
-    show: {
-      get() {
-        return this.value;
+    data: () => ({}),
+    methods: {
+      capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
       },
-      set(value) {
-        this.$emit("input", value);
+      displayLink(link) {
+        var splitLink = link.split(': ');
+        return splitLink[1]
+      }
+    },
+    computed: {
+      show: {
+        get() {
+          return this.value;
+        },
+        set(value) {
+          this.$emit("input", value);
+        },
+      },
+      sortedLeaflet() {
+        return {
+          darreichungsform: this.leaflet.darreichungsform,
+          anwendung: this.leaflet.anwendung,
+          wirkung: this.leaflet.wirkung,
+          verwendung: this.leaflet.verwendung,
+          info: this.leaflet.info,
+          dosierung: this.leaflet.dosierung,
+          nebenwirkungen: this.leaflet.nebenwirkungen,
+          ref: this.leaflet.ref,
+        }
       },
     },
-    
-  },
-  watch: {},
-  methods: {},
-};
+    watch: {},
+  };
 </script>
