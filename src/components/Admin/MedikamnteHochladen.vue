dda421b797bedecce6aed606b6ab33d220d19128
Str4wF4b <fabioalta25@gmail.com>
2022-02-08 03:04:22 +0100

    Oberfläche fertig

--- src/components/Admin/MedikamnteHochladen.vue
+++ src/components/Admin/MedikamnteHochladen.vue
@@ -1,51 +1,100 @@
 <template>
-    <v-card v-if="rights == 3 || rights == 4" style="display:flex">
-        <template>
-            <v-container>
-                <v-row>
-                    <v-file-input v-model="medicamentJson" accept=".json, application/json" label="Json auswählen">
-                    </v-file-input>
-                    <v-file-input v-model="medicamentImage" accept="image/png, image/jpeg, image/bmp"
-                        prepend-icon="mdi-camera" label="Bild auswählen"></v-file-input>
-                </v-row>
-                <v-row>
-                    <div style="display:flex; width:100%">
-                        <v-btn style="margin-left:auto; margin-bottom:15px" id="upload-btn" depressed color="primary"
-                            @click.stop="uploadMedicaments">
-                            Hochladen
-                        </v-btn>
-                    </div>
-                </v-row>
-            </v-container>
-        </template>
-    </v-card>
+  <v-card
+    class="light-blue lighten-5"
+    v-if="rights == 3 || rights == 4"
+    style="display: flex"
+  >
+    <template>
+      <v-container class="light-blue lighten-5">
+        <v-row> <p></p> </v-row>
+        <v-row>
+          <v-col>
+            <v-file-input
+              id="json-input"
+              v-model="medicamentJson"
+              accept=".json, application/json"
+              label="Json auswählen"
+            >
+            </v-file-input>
+          </v-col>
+          <v-col cols="1"> </v-col>
+          <v-col>
+            <v-file-input
+              id="img-input"
+              v-model="medicamentImage"
+              accept="image/png, image/jpeg, image/bmp"
+              prepend-icon="mdi-camera"
+              label="Bild auswählen"
+            ></v-file-input>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <div style="display: flex; width: 100%">
+            <v-btn
+              rounded
+              outlined
+              style="margin-left: auto; margin-bottom: 15px"
+              id="upload-btn"
+              depressed
+              color="primary"
+              @click.stop="uploadMedicaments"
+            >
+              Hochladen
+            </v-btn>
+          </div>
+        </v-row>
+      </v-container>
+    </template>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        data: () => ({
-            medicamentJson: null,
-            medicamentImage: null,
-        }),
-        computed: {
-            rights() {
-                return this.$store.getters.getRights
-            }
-        },
-        methods: {
-            uploadMedicaments() {
-                this.$store.dispatch("readMedicaments", {
-                    json: this.medicamentJson,
-                    image: this.medicamentImage
-                });
-            }
-        },
-    }
+export default {
+  data: () => ({
+    medicamentJson: null,
+    medicamentImage: null,
+  }),
+  computed: {
+    rights() {
+      return this.$store.getters.getRights;
+    },
+  },
+  methods: {
+    uploadMedicaments() {
+      this.$store.dispatch("readMedicaments", {
+        json: this.medicamentJson,
+        image: this.medicamentImage,
+      });
+    },
+  },
+};
 </script>
+<style scoped>
+/* Buttons: */
+#upload-btn {
+  border-color: #2979ff !important;
+  color: #2979ff !important;
+}
+
+#upload-btn:hover {
+  background-color: #2979ff;
+  color: white !important;
+}
+
+/* Input-Fields: */
+#json-input {
+  padding-right: 30px !important;
+}
+
+#img-input {
+  margin-left: 30px !important;
+}
+</style>
 
 <style>
-    #upload-btn {
-        margin-top: auto;
-        margin-left: 20px;
-    }
+#upload-btn {
+  margin-top: auto;
+  margin-left: 20px;
+}
 </style>
--- src/components/Admin/NutzerVerwaltung.vue
+++ src/components/Admin/NutzerVerwaltung.vue
@@ -1,235 +1,407 @@
 <template>
-    <v-card v-if="rights == 3 || rights == 4">
-        <v-data-table :headers="headers" :items="users" item-key="uid" sort-by="uid" group-by="rights"
-            class="elevation-1" show-group-by>
-            <template v-slot:group.header="{items, isOpen, toggle}">
-                <th colspan="4">
-                    <v-icon @click="toggle">{{ isOpen ? 'mdi-minus' : 'mdi-plus' }}
-                    </v-icon>
-                    {{ rechte[items[0].editRights].text }}
-                </th>
-            </template>
-            <template v-slot:item.actions="{ item }">
-                <v-icon small class="mr-2" @click="item.editMode = true" v-if="!item.editMode">
-                    mdi-pencil
-                </v-icon>
-                <v-icon small class="mr-2" @click="saveItem(item)" v-if="item.editMode">
-                    mdi-floppy
-                </v-icon>
-                <v-icon small class="mr-2" @click="stopEditMode(item)" v-if="item.editMode">
-                    mdi-cancel
-                </v-icon>
-                <v-icon small @click="askForDelete(item)" v-if="rights == 4">
-                    mdi-delete
-                </v-icon>
-            </template>
-            <template v-slot:item.name="{ item }">
-                <v-text-field v-model="item.editName" placeholder="Name" :disabled="!item.editMode"></v-text-field>
-            </template>
-            <template v-slot:item.rightsAutocomplete="{ item }">
-                <v-autocomplete v-model="item.editRights" @change="editRightsChanged(item)" :items="rechte" dense
-                    label="Rechte" item-text="text" item-value="index" :disabled="autoDisable(item)">
-                </v-autocomplete>
-            </template></v-data-table>
-        <v-dialog v-model="subjectAreaDialog" transition="dialog-top-transition" max-width="600" persistent>
-            <template>
-                <v-card>
-                    <v-toolbar color="primary" dark>Bitte wählen sie eine Fachrichtung aus.</v-toolbar>
-                    <v-card-text>
-                        <v-select v-model="subjectAreaItems" :items="subjectAreas" item-text="text" item-value="index"
-                            :menu-props="{ maxHeight: '400' }" multiple
-                            hint="Wählen sie die Fachrichtung(en) aus" persistent-hint></v-select>
-                    </v-card-text>
-                    <v-card-actions class="justify-end">
-                        <v-btn text @click="changeSubjectArea">Ok</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </template>
-        </v-dialog>
-        <v-dialog v-model="patientCredentialsDialog" transition="dialog-top-transition" max-width="600" persistent>
-            <template>
-                <v-card>
-                    <v-toolbar color="primary" dark>Bitte tragen sie weitere Informationen zum Patienten ein.
-                    </v-toolbar>
-                    <v-card-text>
-                        <v-text-field v-model="patientCredentials.age" label="Alter"></v-text-field>
-                        <v-select v-model="patientCredentials.sex" :items="sexes" :menu-props="{ maxHeight: '400' }"
-                            label="Select" hint="Geschlecht" persistent-hint></v-select>
-                        <v-text-field v-model="patientCredentials.insurance" label="Versicherung"></v-text-field>
-                    </v-card-text>
-                    <v-card-actions class="justify-end">
-                        <v-btn text @click="changePatientCredentials">Ok</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </template>
-        </v-dialog>
-        <v-dialog v-model="deleteDialog" transition="dialog-top-transition" max-width="600">
-            <template>
-                <v-card>
-                    <v-toolbar color="primary" dark>Nutzer löschen</v-toolbar>
-                    <v-card-text>
-                        Wollen Sie diesen Nutzer wirklich löschen?
-                    </v-card-text>
-                    <v-card-actions class="justify-end">
-                        <v-btn text @click="deleteUser">Löschen</v-btn>
-                        <v-btn text @click="deleteDialog = false">Abbrechen</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </template>
-        </v-dialog>
-    </v-card>
+  <v-card v-if="rights == 3 || rights == 4">
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      item-key="uid"
+      sort-by="uid"
+      group-by="rights"
+      class="elevation-1 light-blue lighten-5"
+      show-group-by
+    >
+      <template v-slot:group.header="{ items, isOpen, toggle }">
+        <th id="th" colspan="4">
+          <v-icon @click="toggle"
+            >{{ isOpen ? "mdi-minus" : "mdi-plus" }}
+          </v-icon>
+          {{ rechte[items[0].editRights].text }}
+        </th>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon
+          small
+          class="mr-2 editIcon"
+          @click="item.editMode = true"
+          v-if="!item.editMode"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          small
+          class="mr-2 saveIcon"
+          @click="saveItem(item)"
+          v-if="item.editMode"
+        >
+          mdi-floppy
+        </v-icon>
+        <v-icon
+          small
+          class="mr-2 stopEditIcon"
+          @click="stopEditMode(item)"
+          v-if="item.editMode"
+        >
+          mdi-cancel
+        </v-icon>
+        <v-icon
+          small
+          class="deleteIcon"
+          @click="askForDelete(item)"
+          v-if="rights == 4"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+      <template v-slot:item.name="{ item }">
+        <v-text-field
+          v-model="item.editName"
+          placeholder="Name"
+          :disabled="!item.editMode"
+        ></v-text-field>
+      </template>
+      <template v-slot:item.rightsAutocomplete="{ item }">
+        <p></p>
+        <v-autocomplete
+          v-model="item.editRights"
+          @change="editRightsChanged(item)"
+          :items="rechte"
+          dense
+          label="Rechte"
+          item-text="text"
+          item-value="index"
+          :disabled="autoDisable(item)"
+        >
+        </v-autocomplete> </template
+    ></v-data-table>
+    <v-dialog
+      v-model="subjectAreaDialog"
+      transition="dialog-top-transition"
+      max-width="600"
+      persistent
+    >
+      <template>
+        <v-card class="light-blue lighten-5">
+          <v-toolbar color="primary" dark
+            >Bitte wählen Sie eine Fachrichtung aus.</v-toolbar
+          >
+          <v-card-text>
+            <v-select
+              v-model="subjectAreaItems"
+              :items="subjectAreas"
+              item-text="text"
+              item-value="index"
+              :menu-props="{ maxHeight: '400', offsetX: true, right: true }"
+              multiple
+              hint="Wählen Sie die Fachrichtung(en) aus"
+              persistent-hint
+            ></v-select>
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn
+              @click="subjectAreaDialog = false"
+              class="abbrechen-buttons"
+              rounded
+              outlined
+              text
+              >Abbrechen</v-btn
+            >
+            <v-btn
+              class="confirm-buttons"
+              rounded
+              outlined
+              text
+              @click="changeSubjectArea"
+              >Bestätigen</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+    <v-dialog
+      v-model="patientCredentialsDialog"
+      transition="dialog-top-transition"
+      max-width="600"
+      persistent
+    >
+      <template>
+        <v-card class="light-blue lighten-5">
+          <v-toolbar color="primary" dark
+            >Bitte tragen Sie weitere Informationen zum Patienten ein.
+          </v-toolbar>
+          <v-card-text>
+            <p></p>
+            <v-text-field
+              v-model="patientCredentials.age"
+              label="Alter"
+            ></v-text-field>
+            <v-select
+              v-model="patientCredentials.sex"
+              :items="sexes"
+              :menu-props="{ maxHeight: '400', offsetX: true, right: true }"
+              label="Geschlecht"
+              persistent-hint
+            ></v-select>
+            <v-text-field
+              v-model="patientCredentials.insurance"
+              label="Versicherung"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn
+              @click="patientCredentialsDialog = false"
+              class="abbrechen-buttons"
+              rounded
+              outlined
+              text
+              >Abbrechen</v-btn
+            >
+            <v-btn
+              class="confirm-buttons"
+              rounded
+              outlined
+              text
+              @click="changePatientCredentials"
+              >Bestätigen</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+    <v-dialog
+      v-model="deleteDialog"
+      transition="dialog-top-transition"
+      max-width="600"
+    >
+      <template>
+        <v-card class="light-blue lighten-5">
+          <v-toolbar color="primary" dark>Nutzer löschen</v-toolbar>
+          <v-card-text class="loeschen-card">
+            Wollen Sie diesen Nutzer wirklich löschen?
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn
+              class="abbrechen-buttons"
+              rounded
+              outlined
+              text
+              @click="deleteUser"
+              >Löschen</v-btn
+            >
+            <v-btn
+              class="confirm-buttons"
+              rounded
+              outlined
+              text
+              @click="deleteDialog = false"
+              >Zurück</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                deleteDialog: false,
-                deleteItem: null,
-                subjectAreaDialog: false,
-                subjectAreaItems: null,
-                patientCredentialsDialog: false,
-                patientCredentials: {
-                    age: null,
-                    sex: null,
-                    insurance: null
-                },
-                sexes: [
-                    "Männlich",
-                    "Weiblich"
-                ],
-                headers: [{
-                        text: 'Name',
-                        align: 'start',
-                        value: 'name',
-                        groupable: false,
-                    },
-                    {
-                        text: 'Rechte',
-                        value: 'rights',
-                        align: 'center'
-                    },
-                    {
-                        text: 'UID',
-                        value: 'uid',
-                        align: 'center',
-                        groupable: false,
-                    },
-                    {
-                        text: 'Rechte',
-                        value: 'rightsAutocomplete',
-                        align: 'center',
-                        groupable: false,
-                    },
-                    {
-                        text: 'Actions',
-                        value: 'actions',
-                        align: 'right',
-                        groupable: false,
-                    },
-                ],
-                rechte: [{
-                        index: 0,
-                        text: "Nicht Verifiziert"
-                    },
-                    {
-                        index: 1,
-                        text: "Patient"
-                    },
-                    {
-                        index: 2,
-                        text: "Arzt"
-                    },
-                    {
-                        index: 3,
-                        text: "Admin"
-                    },
-                    {
-                        index: 4,
-                        text: "Superadmin",
-                        disabled: true
-                    },
-                ],
-            }
+export default {
+  data() {
+    return {
+      deleteDialog: false,
+      deleteItem: null,
+      subjectAreaDialog: false,
+      subjectAreaItems: null,
+      patientCredentialsDialog: false,
+      patientCredentials: {
+        age: null,
+        sex: null,
+        insurance: null,
+      },
+      sexes: ["Männlich", "Weiblich"],
+      headers: [
+        {
+          text: "Name",
+          align: "start",
+          value: "name",
+          groupable: false,
         },
-        mounted() {
-            this.$store.dispatch("fetchUsers")
+        {
+          text: "Rechte",
+          value: "rights",
+          align: "center",
         },
-        computed: {
-            rights() {
-                return this.$store.getters.getRights
-            },
-            users() {
-                return this.$store.getters.getUsers
-            },
-            subjectAreas() {
-                return this.$store.getters.getSubjectAreas
-            }
+        {
+          text: "UID",
+          value: "uid",
+          align: "center",
+          groupable: false,
         },
-        methods: {
-            changePatientCredentials() {
-                if (this.patientCredentials.age && this.patientCredentials.sex && this.patientCredentials.insurance) {
-                    this.patientCredentialsDialog = false
-                }
-            },
-            stopEditMode(item) {
-                item.editMode = false,
-                    item.editRights = item.rights
-                item.editName = item.name
-            },
-            changeSubjectArea() {
-                if (this.subjectAreaItems) {
-                    this.subjectAreaDialog = false
-                }
-            },
-            editRightsChanged(item) {
-                if (item.editRights == 2) {
-                    this.subjectAreaItems = item.subjectArea
-                    this.subjectAreaDialog = true
-                }
-                if (item.editRights == 1) {
-                    this.patientCredentials = {
-                        age: item.age,
-                        sex: item.sex,
-                        insurance: item.insurance
-                    }
-                    this.patientCredentialsDialog = true
-                }
-            },
-            autoDisable(item) {
-                if (item.editRights > this.rights) {
-                    return true
-                } else if (item.editMode == false) {
-                    return true
-                } else {
-                    return false
-                }
-            },
-            askForDelete(item) {
-                console.log(item)
-                this.deleteItem = item
-                this.deleteDialog = true
-                console.log(this.deleteDialog)
-            },
-            deleteUser() {
-                const uid = this.deleteItem.uid
-                this.$store.dispatch("deleteUser", uid)
-                this.deleteDialog = false
-            },
-            saveItem(item) {
-                const name = item.editName
-                const rights = item.editRights
-                const uid = item.uid
-                const subjectArea = this.subjectAreaItems
-                const credentials = this.patientCredentials
-                this.$store.dispatch("updateUser", {
-                    name: name,
-                    rights: rights,
-                    uid: uid,
-                    subjectArea: subjectArea,
-                    credentials: credentials
-                })
-            }
+        {
+          text: "Rechte",
+          value: "rightsAutocomplete",
+          align: "center",
+          groupable: false,
         },
-    }
+        {
+          text: "Actions",
+          value: "actions",
+          align: "right",
+          groupable: false,
+        },
+      ],
+      rechte: [
+        {
+          index: 0,
+          text: "Nicht Verifiziert",
+        },
+        {
+          index: 1,
+          text: "Patient",
+        },
+        {
+          index: 2,
+          text: "Arzt",
+        },
+        {
+          index: 3,
+          text: "Admin",
+        },
+        {
+          index: 4,
+          text: "Superadmin",
+          disabled: true,
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.$store.dispatch("fetchUsers");
+  },
+  computed: {
+    rights() {
+      return this.$store.getters.getRights;
+    },
+    users() {
+      return this.$store.getters.getUsers;
+    },
+    subjectAreas() {
+      return this.$store.getters.getSubjectAreas;
+    },
+  },
+  methods: {
+    changePatientCredentials() {
+      if (
+        this.patientCredentials.age &&
+        this.patientCredentials.sex &&
+        this.patientCredentials.insurance
+      ) {
+        this.patientCredentialsDialog = false;
+      }
+    },
+    stopEditMode(item) {
+      (item.editMode = false), (item.editRights = item.rights);
+      item.editName = item.name;
+    },
+    changeSubjectArea() {
+      if (this.subjectAreaItems) {
+        this.subjectAreaDialog = false;
+      }
+    },
+    editRightsChanged(item) {
+      if (item.editRights == 2) {
+        this.subjectAreaItems = item.subjectArea;
+        this.subjectAreaDialog = true;
+      }
+      if (item.editRights == 1) {
+        this.patientCredentials = {
+          age: item.age,
+          sex: item.sex,
+          insurance: item.insurance,
+        };
+        this.patientCredentialsDialog = true;
+      }
+    },
+    autoDisable(item) {
+      if (item.editRights > this.rights) {
+        return true;
+      } else if (item.editMode == false) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    askForDelete(item) {
+      console.log(item);
+      this.deleteItem = item;
+      this.deleteDialog = true;
+      console.log(this.deleteDialog);
+    },
+    deleteUser() {
+      const uid = this.deleteItem.uid;
+      this.$store.dispatch("deleteUser", uid);
+      this.deleteDialog = false;
+    },
+    saveItem(item) {
+      const name = item.editName;
+      const rights = item.editRights;
+      const uid = item.uid;
+      const subjectArea = this.subjectAreaItems;
+      const credentials = this.patientCredentials;
+      this.$store.dispatch("updateUser", {
+        name: name,
+        rights: rights,
+        uid: uid,
+        subjectArea: subjectArea,
+        credentials: credentials,
+      });
+    },
+  },
+};
 </script>
+
+<style scoped>
+#th {
+  background-color: #cfd8dc;
+}
+
+/* Buttons: */
+.confirm-buttons {
+  border-color: #2979ff !important;
+  color: #2979ff !important;
+}
+
+.confirm-buttons:hover {
+  background-color: #2979ff;
+  color: white !important;
+}
+
+.abbrechen-buttons {
+  border-color: #e53935 !important;
+  color: #e53935 !important;
+}
+
+.abbrechen-buttons:hover {
+  background-color: #e53935 !important;
+  color: white !important;
+}
+
+/* Cards: */
+.loeschen-card {
+    margin-top: 12px;
+}
+
+/* Icons: */
+.editIcon:hover {
+  color: #2979ff;
+}
+
+.saveIcon:hover {
+  color: #aed581;
+}
+
+.stopEditIcon:hover {
+  color: #ef5350;
+}
+
+.deleteIcon:hover {
+  color: red;
+}
+</style>
